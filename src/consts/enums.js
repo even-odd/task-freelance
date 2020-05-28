60713788860823959aa7733d5c6b86ad29ec9e21
even-odd <osadchenko00@inbox.ru>
2020-05-29 01:25:48 +0300

    Добавил новые перечисления

--- src/consts/enums.js
+++ src/consts/enums.js
@@ -5,6 +5,29 @@ export const EProgressBar = {
 
 export const EStatus = {
     Done: 0,
-    Delaye: 1,
-    During: 2
+    Await: 1,
+    Delaye: 2,
+    During: 3
+}
+
+export const EPriority = {
+    Wish: 0,  // хотелка
+    Low: 1, 
+    Medium: 2,
+    Important: 3,
+    Critical: 4   
+}
+
+export const EFilter = {
+    None: 0,
+    ByPriority: 1,
+    ByExecutor: 2,
+    ByStatus: 3
+}
+
+export const ESort = {
+    None: 0,
+    ByPriority: 1,
+    ByExecutor: 2,
+    ByStatus: 3
 }
