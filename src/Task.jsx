5122df8e21d8a600095e5681662ce6524c3a1222
even-odd <osadchenko00@inbox.ru>
2020-05-29 01:25:10 +0300

    Поменял логику работы и добавил новый функционал
    - Сделал заглушки под создание и изменение задачи
    Пока нет четкого понимая как в итоге это будет выглядеть
    - Добавил нормальный вывод приоритетов
    
    P.S плохой коммит, нужно было разбить на несколько

--- src/Task.jsx
+++ src/Task.jsx
@@ -4,52 +4,120 @@ import { getTenDays } from "./consts/anythings";
 
 import PersonSlider from './PersonSlider';
 import ProgressBar from "./ProgressBar";
-import { EProgressBar } from "./consts/enums";
+import { EProgressBar, EPriority, EStatus } from "./consts/enums";
+
+
+// TODO-1-ask: (Task) как правильно сделать изменение значения ?
+// TODO-2-ask: (Task) как происходит обновление компонента после dispatch ?
 
 // Цикл: 
 // new Task -> new AdvancedTask ->? new SimpleTask ->? new Task ....
 
-
-
-// Отображает задачу в главном списке задач
+// Отображает задачу в главном списке задач. 
+// Если нужно создать новую задачу, то используем без передачи props.taskId
+// props:
+//  taskId - по id задача будет получать для себя данные
+//  taskSaved - по факту костыль - не знаю как сообщить родителю о сохраннении задачи
 class Task extends Component {
-    // state - нужен для создания новой задачи
+    constructor(props) {
+        super(props);
+        let testId = props.taskId != null;
 
-    state = {
-        title: '',
-        status: 0, // 0 - ожидает выполнения
-        priority: 0, // 0 - минимальный
-        executors: [],
-        tags: [],
-        timeBegin: new Date(),
-        //будет немного болше чем 10 дней (задержка при выполении + 10 дней от текущего времени)
-        timeEnd: getTenDays(new Date())
+        this.state = {
+            task: (testId) ? this.getTask() : this.сreateNewTask(),
+            newTask: (testId) ? false : true
+        }
     }
 
     render() {
-        let { task } = this.props;
+        let { task } = this.state; 
+        let priority;
+        let priorityClass;
+
+        // TODO-ask: Можно ли это сделать более элегантно ?
+        switch(task.priority) {
+            case EPriority.Wish:
+                priority = 'wish';
+                break;
+            case EPriority.Low:
+                priority = 'low';
+                break;
+            case EPriority.Medium:
+                priority = 'medium';
+                break;
+            case EPriority.Important:
+                priority = 'important';
+                break;
+            case EPriority.Critical:
+                priority = 'crit';
+                break;
+            default:
+                priority = 'default';
+                console.error(`Err: Unknown priority (priority: ${task.priority})`);
+        }
+        priorityClass = `${ priority }-priority`; 
 
         return (
             <div className='task'>
                 <div className='task__info'>
-                    {/* TODO: почитать о создании и выборе классов под ситуации */}
                     <div className='task__props'>
-                        <span className="">
-                            { task.priority }
-                            usual
+                        {/* TODO: Select Priority */}
+                        <span className={ priorityClass }>
+                            { priority }
                         </span>
+
+                        {/* TODO-advanced: Select Tags */}
                     </div>
 
+                    {/* TODO: Change Title */}
                     <span className='task__title'>Title</span>
 
                     <div className="task__progress-bars">
-                        <ProgressBar type={EProgressBar.CompletedTasks} task={this.state}></ProgressBar>
-                        <ProgressBar type={EProgressBar.TimeLimit} task={this.state}></ProgressBar>
+                        <ProgressBar type={EProgressBar.CompletedTasks} task={ task }></ProgressBar>
+                        <ProgressBar type={EProgressBar.TimeLimit} task={ task }></ProgressBar>
                     </div>
                 </div>
-                <PersonSlider persons={ this.state.executors }></PersonSlider>
+
+                {/* TODO: Change Executors */}
+                <PersonSlider persons={ task.executors }></PersonSlider>
+
+                { this.state.newTask && 
+                    <button className='task__btn-save' onClick={ this.saveTask }>Save Task</button>
+                }
             </div>
         )
+    }
+
+    //TODO: Task.getTask 
+    getTask = () => {
+        // получает данные с помощью dispatch и возвращает их
+        // PS пока хз как dispatch передает нам данные
+    }
+
+    //TODO: Task.saveTask 
+    saveTask = () => {
+        // dispatch(saveTask(this.state.task));
+        this.props.taskSaved();
+    }
+
+    сreateNewTask = () => {
+        return {
+            id: this.getFreeId(), // поле необходимо для связи задачи с SimpleTask и AdvancedTask
+            title: '',
+            status: EStatus.Await,
+            priority: EPriority.Wish,
+            executors: [],
+            tags: [],
+            timeBegin: new Date(),
+            timeEnd: getTenDays(new Date()) // будет немного больше чем 10 дней (задержка при выполении + 10 дней от текущего времени)
+        };
+    }
+
+    // TODO: Task.getFreeId
+    // Id должно резервироваться в отдельном хранилище,
+    // Только тогда, когда задача будет создана, произойдет запись в БД
+    getFreeId = () => {
+         
     }
 }
 
