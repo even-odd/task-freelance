a03e088cd77c3742f88e93e0580ffe95b17cc131
even-odd <osadchenko00@inbox.ru>
2020-05-30 00:15:15 +0300

    Заменил заглушку SimpleTask-a

--- src/SimpleTask.jsx
+++ src/SimpleTask.jsx
@@ -1,23 +1,59 @@
 import React, { Component } from "react";
+import { EStatus } from "./consts/enums";
 
 // Служит для ображения простого пункта задачи
 // Можно будет создать задачу отображаемую в основном списке
+// props:
+//  taskId - по id задача будет получать для себя данные
+//  newTask - флаг создания новой задачи
+
+
+// TODO-tmp: SimpleTask data 
+// state = {
+//     title: '',
+//     status: 0
+// }
 class SimpleTask extends Component {
-    // state - нужен для создания новой задачи
-    state = {
-        title: '',
-        status: 0
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            simpleTask: (props.newTask) 
+            ? this.сreateNewTask(props.taskId)
+            : this.getSimpleTask(props.taskId),
+        }
     }
 
     render() {
+        let { title, status } = this.state.simpleTask; 
         return (
-           <div className='advancedTask'>
-                AdvancedTask
-                simpleTaskList.map((simpleTask) => 
-                    <SimpleTask></SimpleTask>
-                )
-           </div> 
+           <div className='sl-task'>
+                <input type="checkbox" 
+                    name={`simple_${this.props.taskId}`} 
+                    id={`simple_${this.props.taskId}`}/>
+
+                <label htmlFor={`simple_${this.props.taskId}`}>
+                    { title }
+                </label>
+
+                <span className='sl-task__status'>
+                    { status }
+                </span>
+           </div>
         );
+    }
+
+    //TODO: SimpleTask.getSimpleTask 
+    getSimpleTask = (taskId) => {
+
+    } 
+
+    createNewTask = (taskId) => {
+        return {
+            id: taskId, // поле необходимо для связи задачи с SimpleTask и AdvancedTask
+            title: '',
+            status: EStatus.Await
+        };
     }
 }
 
