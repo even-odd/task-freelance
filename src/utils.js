445dfb979a7941cb29cd53511b404842e8dfc06d
even-odd <osadchenko00@inbox.ru>
2020-06-10 21:35:21 +0300

    Добавил функции в utils

--- src/utils.js
+++ src/utils.js
@@ -21,11 +21,11 @@ export function getProperty(propName, propValue, propTitles, propClasses) {
     switch(locPropName) {
         case 'priority':
             locEnum = EPriority;
-            console.debug('Enum: Priority');// debug
+            // console.debug('Enum: Priority');// debug
             break;
         case 'status':
             locEnum = EStatus;
-            console.debug('Enum: Status');// debug
+            // console.debug('Enum: Status');// debug
             break;
         default: 
             let err = `Err: utils.getProperty - Unknown Enum (enum: ${propName})`;
@@ -35,18 +35,14 @@ export function getProperty(propName, propValue, propTitles, propClasses) {
     }
 
     let enumKeys = Object.keys(locEnum);
-    
-    printArrValues(enumKeys);// debug
 
     for(let i = 0; i < enumKeys.length; i++) {
-        if(enumKeys[i].localeCompare(propValue) === 0) {
-            console.debug(`Property: ${propValue},
-                            Title: ${propTitles[i]},
-                            Class: ${propClasses[i]}`);// debug
+        if(locEnum[enumKeys[i]] === propValue) {
+            // console.debug(`Property: ${propValue},\nTitle: ${propTitles[i]},\nClass: ${propClasses[i]}`);// debug
             result.title = propTitles[i];
             result.class = propClasses[i];
             return result;
-        }
+        } 
     }
 
     let err = `Err: Unknown ${propName} (${propName}: ${propValue})`;
@@ -66,11 +62,8 @@ export function printArrValues(arr) {
 // debug function
 export function printObjValues(obj) {
     console.debug('PRINT OBJ VALUES - begin');
-    let keys = Object.keys(obj);
-
-    for(let i = 0; i < keys.length; i++) {
-        console.log(`Key: ${keys[i]}, Value: ${obj[keys[i]]}`);
-    }
+        for(let prop in obj) 
+            console.log(`Prop: ${prop}, Value: ${obj[prop]}`);
     console.debug('PRINT OBJ VALUES - end');
 }
 
@@ -82,23 +75,140 @@ export function numberTest(name, value, from, throwErr = false) {
     let msg;
     let hasErr = false;
 
-    if(!value) {
+    if(!value && value !== 0) {
+        hasErr = true;
         msg = `Err: ${from} - ${name} does't exist`;
     } else if(value <= -1) {
+        hasErr = true;
         msg = `Err: ${from} - invalid ${name}`;
     }
 
+    throwOrPrint(msg, hasErr, throwErr);
+
+    return true;
+}
+
+// Делает проверку массива
+// return: true - проверка пройдена
+//         fasle - проверка НЕ пройдена 
+export function arrTest(name, arr, from, throwErr = false) {
+    let msg;
+    let hasErr = false;
+
+    if(arr === undefined) {
+        hasErr = true;
+        msg = `Err: ${from} - ${name} does't exist`;
+    } else if(arr.length <= 0) {
+        msg = `Warn: ${from} - ${name} is empty`;
+    }
+
+    throwOrPrint(msg, hasErr, throwErr);
+
+    return true;
+}
+
+function throwOrPrint(msg, hasErr, throwErr) {
     if (hasErr && throwErr) {
         throw new Error(msg);
     } else if(hasErr){
         console.error(msg);
         return false;
-    } 
-
-    return true;
+    }
 }
 
+// Миллесекунды в дни
 export function getDays(milliseconds) {
     // msInSec / secInMin / minInHour / hourInDay 
     return Math.floor(milliseconds / 1000 / 60 / 60 / 24);
+}
+
+const TEN_DAYS = 10 * 24 * 60 * 60 * 1000; 
+export function getTenDays(currTime) {  
+    return currTime + TEN_DAYS;
+}
+
+// Вычислет свободный id
+// args: arrId - массив занятых id
+export function getFreeId(arrId) {
+    let lastId = (arrId.length > 0) ? arrId[arrId.length - 1] : 0;
+    let count = 0;
+
+    if(arrId[arrId.length - 1] && lastId === 0) {
+        // console.debug(`     .getFreeId() - newId: ${lastId}`);
+        return lastId;
+    }
+
+    while(true) {
+        if(arrId.includes(lastId + count)) {
+            count++ 
+        } else {
+            // console.debug(`     .getFreeId() - newId: ${lastId + count}`);
+            return lastId + count;
+        }
+    }
+}
+
+// Date в удобочиемый вид
+export function getStrDate(date) {
+    return `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`
+}
+
+// Str в Date
+export function parseStrToDate(strToDate) {
+    let match = strToDate.match(/(\d?\d)-(\d?\d)-(\d{4})/);
+    
+    let parsed = new Date();
+    parsed.setDate(+match[1]);
+    parsed.setMonth(match[2] - 1);
+    parsed.setFullYear(+match[3]);
+
+    return parsed;
+}
+
+// Делает валидацию и подготавливает значение к изменению 
+export function prepareChangedProp(prop, rawValue) {
+    console.debug('prepareChangedProp() - prop:', prop);
+    let validated = {hasErr: false, trueFormat: 'default format'};
+    let doPrepare; 
+    let prepared = {
+        value: '',
+        err: false
+    };
+
+    switch(prop) {
+        // для строковых значений - никаких изменений
+        case 'title':
+            // validated = validateTaskTitle(rawValue, state);
+            doPrepare = () => rawValue;
+            break;
+        case 'timeBegin':
+        case 'timeEnd':
+            validated = validateTime(rawValue);
+            doPrepare = () => parseStrToDate(rawValue);
+            break;
+        // для числовых значений - приводим к числу
+        default: 
+            doPrepare = () => +rawValue;
+    }
+
+    if(validated.hasErr) {
+        prepared.value = `Invalid ${prop} format, use - ${validated.trueFormat}`;
+        prepared.err = true;
+    } else {
+        prepared.value = doPrepare();
+    }
+
+    return prepared;
+}
+
+// Делает валидацию времени
+export function validateTime(time) {
+    let validated = {
+        hasErr: false,
+        trueFormat: /\d+-\d+-\d{4}/
+    };
+
+    if(!validated.trueFormat.test(time)) validated.hasErr = true;
+
+    return validated;
 }
