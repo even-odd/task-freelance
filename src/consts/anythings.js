a3277f2b712ee945ccb8de4606401dc5049a3eee
even-odd <osadchenko00@inbox.ru>
2020-06-10 19:46:36 +0300

    Удалил ненужное

--- src/consts/anythings.js
+++
@@ -1,6 +0,0 @@
-
-// daysAmount * dayHours * oneHourInMinutes * oneMinuteInSec * oneSecInMillsec 
-const TEN_DAYS = 10 * 24 * 60 * 60 * 1000; 
-export function getTenDays(currTime) {  
-    return currTime + TEN_DAYS;
-}
