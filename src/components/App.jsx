090f13c1d9d61d4e694338fd4675368f50d29712
even-odd <osadchenko00@inbox.ru>
2020-06-15 00:50:13 +0300

    Внес "сахарные" изменения в App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+
 import './App.css';
 
-import TaskList from './TaskList/TaskList'; 
 import TaskFilter from './TaskFilter/TaskFilter';
+import TaskList from './TaskList/TaskList'; 
 
 function App() {
   return (
     <div className="app">
-      <TaskFilter></TaskFilter>
-      <TaskList></TaskList>
+      <TaskFilter/>
+      <TaskList/>
     </div>
   );
 }
