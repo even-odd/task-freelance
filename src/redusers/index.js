a70b72aa1f808ac9e613a5a2fbe36f998dc79d2c
even-odd <osadchenko00@inbox.ru>
2020-06-15 00:55:27 +0300

    "Гламурное" изменение переменной

--- src/redusers/index.js
+++ src/redusers/index.js
@@ -3,7 +3,7 @@ import { combineReducers } from 'redux';
 import { tasks } from './tasks';
 import { activeFilter } from './activeFilter';
 
-export const appRedusers = combineReducers({
+export const rootReduser = combineReducers({
     tasks,
     activeFilter
 });
